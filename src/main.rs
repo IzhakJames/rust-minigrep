6f8920d52f7b3539394c58486f2b31b55940bb48
Izhak James <hein_htet_23@hotmail.com>
2025-10-26 11:27:06 +0800

    Change return type of build to result

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|error| {
+        println!("Problem parsing arguments: {error}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,8 +24,13 @@ struct Config {
     file_path: String
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config {query, file_path}
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Please input at least 2 arguments e.g search_word file_path");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config {query, file_path})
+    }
 }
